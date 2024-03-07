833303827e84ec7205564ab7b4db2b5f357b59d7
Björn Sundman <bjorn.sundman@yahoo.com>
2024-03-07 03:35:49 +0100

    trying to get fastapi to work

--- FastAPI/models.py
+++ FastAPI/models.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Column, Integer, String, Float, Boolean, ForeignKey
-from database import Base
+from sqlalchemy import Column, Integer, String, Float, Boolean, ForeignKey, Table
+from database import Base  
 from sqlalchemy.orm import relationship
+from pydantic import BaseModel
 
 
 class User(Base):
@@ -9,32 +10,30 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, index=True)
     email = Column(String, unique=True, index=True)
-    password_hash = Column(String)
+    password = Column(String)
+    groups = relationship("Group", secondary="user_group", back_populates="members")
 
 class Group(Base):
     __tablename__ = "groups"
 
     id = Column(Integer, primary_key=True)
     name = Column(String, unique=True)
-    # Add other group attributes as needed
-
     members = relationship("User", secondary="user_group", back_populates="groups")
-
 
 class Transaction(Base):
     __tablename__ = "transactions"
     
     id = Column(Integer, primary_key=True, index=True)
     amount = Column(Float)
-    category = Column(String)
-    description = Column(String)
-    is_income = Column(Boolean)
+    share = Column(String)
     date = Column(String)
     
+class Share(Base):
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True)
+    price = Column(Float)
+    member = relationship("User")
     
-user_group = Table(
-    "user_group",
-    Base.metadata,
-    Column('user_id', Integer, ForeignKey('users.id')),
-    Column('group_id', Integer, ForeignKey('groups.id'))
-)
+class UserLogin(BaseModel):
+    email: str
+    password: str
